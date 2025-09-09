ff6adee563772a2c94478ef9eb5a14c444110bfc
Julius-3367 <korirjulius001@gmail.com>
2025-09-09 22:38:22 +0300

    wk-4 assignement

---
+++ answers.sql
@@ -0,0 +1,32 @@
+-- Question 1
+SELECT 
+    paymentDate,
+    SUM(amount) AS total_payment
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+-- Question 2
+SELECT 
+    customerName,
+    country,
+    AVG(creditLimit) AS avg_credit_limit
+FROM customers
+GROUP BY customerName, country;
+
+-- Question 3
+SELECT 
+    productCode,
+    quantityOrdered,
+    SUM(quantityOrdered * priceEach) AS total_price
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+
+-- Question 4
+SELECT 
+    checkNumber,
+    MAX(amount) AS highest_payment
+FROM payments
+GROUP BY checkNumber;
+
